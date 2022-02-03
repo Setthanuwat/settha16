9ff23ea7ef97dd3efca49ef1ef633d2e6161fee5
Setthanuwat <aftnick2@gmail.com>
2022-02-04 02:36:57 +0700

    "Update"

--- lab22.h
+++ lab22.h
@@ -63,7 +63,66 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead()
+{
+	if(hp  <= 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
+void Unit:: guard()
+{
+	guard_on = true;
+}
+
+int Unit::beAttacked(int oppatk)
+{
+	int dmg;
+	if(guard_on)
+	{
+		dmg = (oppatk - def)/3;
+	}
+	else
+	{
+		dmg = (oppatk - def);
+	}
+	if(dmg < 0)
+	{
+		dmg = 0;
+	}
+	hp = hp - dmg;
+	return dmg;
+}
+
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
+
+int Unit::heal()
+{
+	int H = rand()%21+10;
+	if(hp == hpmax)
+	{
+		H = 0;
+	}
+	else if(hp + H > hpmax)
+	{
+		H = hpmax - hp;
+		hp = hp+H;
+	}
+	else
+	{
+		hp = hp + H;
+	}
+	
+
+	return H ;
+}	
 
 /////////////////////////////////////////////////////////////////////////////////////
 //Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
